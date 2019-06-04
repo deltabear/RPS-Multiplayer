773323497788eb796928d919773b773e0b61f91a
max delzer <emma.delzer@gmail.com>
2019-06-03 18:52:15 -0700

    Finished pseudocoding game script

--- assets/javascript/rpsApp.js
+++ assets/javascript/rpsApp.js
@@ -1,5 +1,15 @@
-// Create a two-player game of Rock Paper Scissors using Firebase.    
-    
+// Create a two-player game of Rock Paper Scissors using Javascript and Firebase.
+//Initialize Firebase
+// Players will input their names and have it displayed as long as they are playing
+  // Create variables to hold choices that both users can input.
+  // Create variables to hold number of wins, losses, and draws. (start at 0)
+  // Create variables to hold references to places in HTML where they should be displayed
+  // Create a function that determines when both users have pressed a key, and will ONLY display
+    // the choices after both users have made their choice
+  //After both choices are made, game will announce [Player NAME] is the WINNER!
+  // Game will increment wins for victorious player, and increment losses for losing player.
+  // Create an in-game chatting system that allows both players to chat with each other on the page
+    // User types out message (with character limit?) in chat field, presses submit, and page will display it in chat feed
     
     
     // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
@@ -75,3 +85,4 @@
         lossesText.textContent = "losses: " + losses;
         tiesText.textContent = "ties: " + ties;
       }
+    }
